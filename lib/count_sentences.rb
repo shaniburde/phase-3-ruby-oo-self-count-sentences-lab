10c369e31e0e7f47d1b4fd5638de9ac78962aa4c
shani <shhhaniburde@gmail.com>
2022-02-01 21:34:47 -0500

    commit tree b0aa2a1d9662588f1058dd52d6330d62bebe761c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+      self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
+    # non-regular expression solution:
+    sentences = 0
+    # what punctuation we're checking for
+    punctuation = [".", "?", "!"]
+    # iterate over every character in the string
+    self.chars.each.with_index do |char, index|
+      # check if the character is a punctuation, and if the _next_ character is _not_ a punctuation
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
 end
